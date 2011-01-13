b5803354dee445c783bcdf817b0e317b0858430a
Dillon Hafer <dhafer@hylesanderson.edu>
2011-01-13 15:26:24 -0600

    fixed application.erb syntax.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    @base_title = "dhafer.com"
+  end
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -12,7 +16,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                      :content => "dhafer | Home")
+                      :content => @base_title + " | Home")
     end
   end
 
@@ -25,7 +29,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                      :content => "dhafer | Contact")
+                      :content => @base_title + " | Contact")
     end
   end
 
@@ -38,7 +42,7 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                      :content => "dhafer | About") 
+                      :content => @base_title + " | About") 
     end
   end
                       
@@ -51,7 +55,7 @@ describe PagesController do
     it "should have the right title" do
       get 'help'
       response.should have_selector("title",
-                      :content => "dhafer | Help")
+                      :content => @base_title + " | Help")
     end
   end
 end
