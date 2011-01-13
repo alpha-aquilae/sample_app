692ded6d8b466e77cfd59c887d3473a5e2b41580
Dillon Hafer <dhafer@hylesanderson.edu>
2011-01-13 10:32:29 -0600

    finished static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end  
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                      :content => "dhafer | Home")
     end
   end
 
@@ -13,7 +20,25 @@ describe PagesController do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end 
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                      :content => "dhafer | Contact")
     end
   end
 
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                      :content => "dhafer | About")
+    end
+  end
 end
