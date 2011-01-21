de4e9c97149118a24392b0aea5ec075cf7fecc56
Dillon Hafer <dhafer@hylesanderson.edu>
2011-01-21 15:50:01 -0600

    finished chapter five

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+  
   # return a title on a per-page basis
   def title
     base_title = "dhafer.com"
