8d3d48fb39f7ba77c5b3e38dde2d1a7ad5b5c004
Dillon Hafer <dhafer@hylesanderson.edu>
2011-02-04 10:21:55 -0600

    finished ch7 finally

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,12 +3,30 @@ require 'spec_helper'
 describe UsersController do
   render_views
 
-  describe "GET 'new'" do
-  
   before(:each) do
     @user = Factory(:user)
   end
   
+  describe "GET 'show'" do
+  
+    it "should have the right title" do
+      get :show, :id => @user
+      response.should have_selector("title", :content => @user.name)
+    end
+
+    it "should include the user's name" do
+      get :show, :id => @user
+      response.should have_selector("h1", :content => @user.name)
+    end
+    
+    it "should have a profile image" do
+      get :show, :id => @user
+      response.should have_selector("h1>img", :class => "gravatar")
+    end
+  end
+  
+  describe "GET 'new'" do
+    
     it "should be successful" do
       get :show, :id => @user
       response.should be_success
@@ -24,4 +42,5 @@ describe UsersController do
       get 'new'
       response.should have_selector("title", :content => "Sign up")
     end
+    
 end
