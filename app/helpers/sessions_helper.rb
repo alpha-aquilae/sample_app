1103fe28c3c4ba098958133ca6dc67018bbb5756
Dillon Hafer <dhafer@hylesanderson.edu>
2011-02-23 14:19:36 -0600

    another update

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,8 +4,27 @@ module SessionsHelper
     cookies.permanent.signed[:remember_token] = [user.id, user.salt]
     self.current_user = user
   end
-
-  def current_user=(user)
-    @current_user = user
+  
+  def sign_out
+    cookies.delete(:remeber_token)
+    self.current_user = nil
   end
+  
+  def current_user
+    @current_user = user ||= user_from_remember_token
+  end
+  
+  def signed_in?
+    !current_user.nil?
+  end
+  
+  private
+  
+    def user_from_remember_token
+      User.authenticate_with_salt(*remeber_token)
+    end
+  
+    def remeber_token
+      cookie.signed[:remember_token] || [nil, nil]
+    end
 end
